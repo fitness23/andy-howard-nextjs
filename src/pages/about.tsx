04bc3ef518d7a5eb96c1fe8d028d596cbda45972
fitness23 <ajfhoward@hotmail.com>
2022-06-08 16:29:36 +0100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ import { Main } from '@/templates/Main';
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+      Lorem ipsum2 dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
       voluptatibus distinctio recusandae autem esse explicabo molestias officia
       placeat, accusamus aut saepe.
